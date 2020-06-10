c96aa8cdd2bdea06b3a0035721b0678170bf2c16
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-06-09 21:42:02 -0500

    Completed Merge but not sure if it works yet. There are no errors though

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,17 +2,45 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
+    #Left
+    x = 0
+    #right
+    y = 0
+    #new array
+    z = 0
 
     # Your code here
-
+    #This will traverse over both of the arrays
+    while x < len(arrA) and y < len(arrB):
+        #We need to check if the current element of the first
+        #array is smaller than the curr of the second array
+        if arrA[x] < arrB[y]:
+            merged_arr[z] = arrA[x]
+            z = z + 1
+            x = x + 1
+        else:
+            merged_arr[z] = arrB[y]
+            z = z + 1
+            y = y + 1
+    #THESE WHILE LOOPS WILL STORE THE REMAINING ELEMENTS
+    while x < len(arrA):
+        merged_arr[z] = arrA[x]
+        z = z + 1
+        x = x + 1
+    while y < len(arrB):
+        merged_arr[z] = arrB[x]
+        z = z + 1
+        y = y + 1
+    #range returns a sequence of numbers starting at 0
+    for x in range(elements):
+        print(str(merged_arr[x]))
 
     return merged_arr
-
+    print(merged_err)
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
-
 
     return arr
 
