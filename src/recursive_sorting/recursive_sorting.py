abaaecdb46fc5291afeab2c40a57f59d2fd8ccfb
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-06-09 21:49:35 -0500

    Fixing some bugs.

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,19 +1,22 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+# arrA = [1, 3, 5, 7]
+# arrB = [2, 4, 6, 8]
+
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-    #Left
+    # Left
     x = 0
-    #right
+    # right
     y = 0
-    #new array
+    # new array
     z = 0
 
     # Your code here
-    #This will traverse over both of the arrays
+    # This will traverse over both of the arrays
     while x < len(arrA) and y < len(arrB):
-        #We need to check if the current element of the first
-        #array is smaller than the curr of the second array
+        # We need to check if the current element of the first
+        # array is smaller than the curr of the second array
         if arrA[x] < arrB[y]:
             merged_arr[z] = arrA[x]
             z = z + 1
@@ -22,7 +25,7 @@ def merge(arrA, arrB):
             merged_arr[z] = arrB[y]
             z = z + 1
             y = y + 1
-    #THESE WHILE LOOPS WILL STORE THE REMAINING ELEMENTS
+    # THESE WHILE LOOPS WILL STORE THE REMAINING ELEMENTS
     while x < len(arrA):
         merged_arr[z] = arrA[x]
         z = z + 1
@@ -31,12 +34,12 @@ def merge(arrA, arrB):
         merged_arr[z] = arrB[x]
         z = z + 1
         y = y + 1
-    #range returns a sequence of numbers starting at 0
+    # range returns a sequence of numbers starting at 0
     for x in range(elements):
         print(str(merged_arr[x]))
 
     return merged_arr
-    print(merged_err)
+
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
@@ -49,13 +52,11 @@ def merge_sort(arr):
 def merge_in_place(arr, start, mid, end):
     # Your code here
 
-
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
 
     return arr
 
